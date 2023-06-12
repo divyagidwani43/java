bb277824693b2d6bd0662468c845d41497b8b2dd
divya gidwani <96631510+divyagidwani43@users.noreply.github.com>
2023-06-12 22:14:02 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 2 slashes indicate comment
+
+        System.out.println("hi"+2);// is the way to print anything ,
+        // short form is sout
+
+        //+ sign joins lines with different data types in sout
+
+        //!= means not equal to
+
+        //we can even out spacing by code --> reformat code
+
+        // we can reformat code by code--> reformat code
+
+        int myVar =5; char myChar ='i'; System.out.println(myVar+myChar);//doubt
+
+        //-1 is used mostly to indicate value not found or error , can also be used as int or return value
+
+        //to change code to comment use shortcut ctrl+/ or code-->comment with line comment
+
+        //we need a main method in java to run or test the code i.e we need public static void main or use short form psvm
+
+    }
+
+}
